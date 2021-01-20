80575a021326d10b5fe2af66971fc9ae1397dc78
Alexandre Ferreira <alexandreferreiracontact@gmail.com>
2021-01-20 18:13:37 -0300

    criando primeiro controller

--- public/index.php
+++ public/index.php
@@ -10,8 +10,8 @@ if (preg_match('/\.(?:png|jpg|jpeg|gif)$/', $_SERVER["REQUEST_URI"])) {
   define('DEFAULT_CONTROLLER', 'home');
   define('DEFAULT_METHOD', 'index');
 
-  require '././vendor/autoload.php';
-  require '././App/Functions/functions_twig.php';
+  require '../vendor/autoload.php';
+  require '../App/Functions/functions_twig.php';
   require 'bootstrap/bootstrap.php';
 
 }
