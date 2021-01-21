f55dfcb65a05d7f4e1e63807e95b9c3dcfdaa0fc
Alexandre Ferreira <alexandreferreiracontact@gmail.com>
2021-01-21 16:04:02 -0300

    chamando controller com method

--- App/Controllers/Site/ProdutoController.php
+++ App/Controllers/Site/ProdutoController.php
@@ -2,5 +2,10 @@
 namespace App\Controllers\Site;
 
 class ProdutoController {
-
+  public function index() {
+    dump("index");
+  }
+  public function calca() {
+    dump("calca");
+  }
 }
--- public/bootstrap/bootstrap.php
+++ public/bootstrap/bootstrap.php
@@ -1,6 +1,17 @@
 <?php
-$controller = new App\Controllers\Controller;
-$object = $controller->controller();
-$method = new App\Controllers\Method;
+/**
+ * Chamando o Controller digitado na url
+ */
+$callController = new App\Controllers\Controller;
+$calledController = $callController->controller();
+$controller = new $calledController();
 
-dump($method->method($object));
+/**
+ * Chamando o método digitado na url
+ */
+$callMethod = new App\Controllers\Method;
+$method = $callMethod->method($controller);
+/**
+ * Chamando o controller atraves da classe controller e da classe method
+ */
+$controller->$method();
